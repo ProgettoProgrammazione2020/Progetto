8f02f0cede2fe5ae1ef14e71e93300fc7ce0b7dd
Pastro02 <marc.pastrello@gmail.com>
2020-05-11 16:17:39 +0200

    cambiamento get e set

--- test_main_priv.c
+++ test_main_priv.c
@@ -5,13 +5,17 @@
 
 int main(){
 
-    char *source = "flower.bmp";
+    char *source = "flower2.bmp";
     char *destination = "B&W_flower.bmp";
     int success;
 
     Bitmap *test1 = bm_load(source); //carico file input
     ip_mat *test1_ipmat = bitmap_to_ip_mat(test1); //converto in ip_mat
-    ip_mat *result = ip_mat_to_gray_scale(test1_ipmat); //CONVERTO IN SCALA DI GRIGI
+    
+    
+    
+    ip_mat *result = ip_mat_corrupt(test1_ipmat,100); //CONVERTO IN SCALA DI GRIGI
+    //clamp(result, 0, 255);
     Bitmap *out = ip_mat_to_bitmap(result);//converto in bitmap
     success = bm_save(out,destination);//salvo l'immagine su filter
 
