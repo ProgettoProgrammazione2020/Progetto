fdf96d29c30b7b73c1e2e3c237869119d8efe2ef
Niccolò Simonato <879993@stud.unive.it>
2020-05-11 12:47:35 +0200

    Aggiornato main di prova

--- test_main_priv.c
+++ test_main_priv.c
@@ -5,15 +5,19 @@
 
 int main(){
 
-    Bitmap *test1 = bm_load("flower.bmp"); //carico file input
+    char *source = "flower.bmp";
+    char *destination = "B&W_flower.bmp";
+    int success;
+
+    Bitmap *test1 = bm_load(source); //carico file input
     ip_mat *test1_ipmat = bitmap_to_ip_mat(test1); //converto in ip_mat
     ip_mat *result = ip_mat_to_gray_scale(test1_ipmat); //CONVERTO IN SCALA DI GRIGI
     Bitmap *out = ip_mat_to_bitmap(result);//converto in bitmap
-    bm_save(out,"TEST1.bmp");//salvo l'immagine su filter
+    success = bm_save(out,destination);//salvo l'immagine su filter
 
     ip_mat_free(test1_ipmat);
     ip_mat_free(result);
     bm_free(test1);
     bm_free(out); //libero tutta la memoria dinamica
-    return 0;
+    return success;
 }
